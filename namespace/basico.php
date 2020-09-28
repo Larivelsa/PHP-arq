80c78b07ec354f2e136442b594dac7a570f73cfe
Larissa <larissavelsa@gmail.com>
2020-09-28 16:57:01 -0300

    --

--- namespace/basico.php
+++ namespace/basico.php
@@ -1,3 +1,40 @@
+<?php namespace contexto;?> <!-- precisa ser a primeira sentença de código o namespace -->
+
 <div class="titulo">Exemplo Básico</div>
+
+<?php 
+/* constante para referenciar o namespace __NAMESPACE__*/
+echo __NAMESPACE__ . '<br>';
+
+/* declaração de constante não usa o sinal $, usa o namespace local como padrão*/
+const CONSTANTE1 = 123;
+
+/* declaração de constante com "define" precisa declarar o namespace $*/
+define('contexto\CONSTANTE2',234);
+
+define(__NAMESPACE__.'\CONSTANTE3',345);
+
+echo CONSTANTE1 .'<br>';
+echo CONSTANTE2.'<br>';
+
+echo \contexto\CONSTANTE3.'<br>'; /**caminho absoluto */
+
+/** name space é para também funções dentre outros */
+function soma($a,$b){
+   return $a + $b; 
+}
+
+echo \contexto\soma(1,2).'<br>';
+echo soma(1,2).'<br>';
+
+function strpos($str,$text){
+    echo "Buscando o texto '{$text} em '{$str}'<br>";
+    return 1;
+}
+
+
+
+
+
 
 
